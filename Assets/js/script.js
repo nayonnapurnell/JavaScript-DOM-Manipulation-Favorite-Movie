a993f57909ef3f8c8b22cbc26cb8cad580d0c618
“Nayonna <Nayonna Purnell>
2022-10-24 07:21:27 -0400

    Added the DOM manipulation calls

---
+++ Assets/js/script.js
@@ -0,0 +1,24 @@
+//DOM Manipulation
+
+// GetElementById()
+//Place the selector inside a variable so that we can manipulate it.
+var  title = document.getElementById('main-heading');
+console.log(title);
+
+// GetElementByClassName()
+// var listItems = document.getElementsByClassName('list-items');
+// console.log(listItems);
+
+// GetElementsByTagName()
+// var listItems = document.getElementsByTagName('li');
+// console.log(listItems);
+
+// querySelector()
+//querySelector selects the 1st item that matches that criteria
+// var container = document.querySelector('div');
+// console.log(container);
+
+// querySelectorAll()
+var container = document.querySelectorAll('div');
+console.log(container);
+ 
